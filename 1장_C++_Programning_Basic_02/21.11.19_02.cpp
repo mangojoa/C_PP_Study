cbb7956fe05d16bd68506fc361b4039db626a612
mangojoa <sxfc675@gmail.com>
2021-11-19 23:35:39 +0900

    [2021.11.19] cin / >>

--- 1장_C++_Programning_Basic_02/21.11.19_02.cpp
+++ 1장_C++_Programning_Basic_02/21.11.19_02.cpp
@@ -19,7 +19,29 @@ std:: 란 ?? 표준 이름 공간을 의미한다.
 
 int main() {
 
-	cout << "hello, Would!!";
+	/*
+	cin 과 >> 연산자를 이용한 키 입력
+
+	<iostream> 안에 cin 과 >> 연산자가 파일 안에 있다. 
+	cin은 키보드와 C++ 응용프로그램을 연결하는 C++ 표준 입력 스트임 객체이다.
+	응용 프로그램은 cin 객체로부터 입력된 키 값을 읽는다. 
+
+	>>은 스트림 추출 연산자로 불리며 왼쪽 피연산자인 스트림 객체로부터 데이터를 읽어 
+	오른쪽 피연산자에 지정된 변수에 삽입한다. 
+	C++의 기본 연산자이지만 <iostream> 헤더 파일에 스트림 추출 연산자로 
+	재정의(overloading)되어 있다. 
+	*/
+
+	cout << "너비를 입력하세요 >> ";
+	int width;
+	cin >> width; // 키보드로부터 정수 값 너비를 읽어 width 변수에 저장
+
+	cout << "높이를 입력하세요 >> ";
+	int height;
+	cin >> height; // 키보드로부터 정수 값 높이를 읽어 height 변수에 저장
+
+	int area = width * height; // 면적을 계산 
+	cout << "면적은 >> " << area << endl;
 
 	return 0;
 }
