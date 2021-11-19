6425e8e6313e773cda48b01319f88458b95ab2d7
mangojoa <sxfc675@gmail.com>
2021-11-19 22:45:04 +0900

    [2021.11.19] 변경사항 저장

---
+++ 1장_C++_Programning_Basic_02/21.11.19_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+/*
+C++ 프로그레밍 과정에서 이름(identifier)이 충돌하는 경우가 있다.
+협업을 하는 경우 다양한 이름을 생성하면서 충돌이 발생하곤 한다.
+이를 방지 하기 위해 이금 공간을 생성할 수 있도록 namespase 키워드를 도입
+서로 다른
+*/
+using namespace std;
+
+int main() {
+
+
+
+	return 0;
+}
