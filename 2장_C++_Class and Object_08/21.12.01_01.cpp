e5760c98465e72c67046e47a60cf674449b1616f
mangojoa <sxfc675@gmail.com>
2021-12-01 23:55:26 +0900

    [21.12.01] 접근 지정자

---
+++ 2장_C++_Class and Object_08/21.12.01_01.cpp
@@ -0,0 +1,23 @@
+/*
+접근지정자 
+
+private / public / protected 등이 존재한다.
+
+private 맴버
+클래스 내의 맴버 함수들에게만 접근이 허용된다.
+
+public 맴버 
+클래스 내외를 막론하고 프로그램의 모든 함수들에게 접근이 허용된다.
+
+protected 맴버
+클래스 내의 맴버 함수와 이 클래스를 상속받은 파생 클래스의 맴버 함수에게만 접근이 허용된다.
+
+class Sample {
+private:
+	
+public:
+
+protected:
+
+}
+*/
