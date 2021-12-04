bdf58a82500656c0959307c14a06e8bca5beea57
mangojoa <sxfc675@gmail.com>
2021-12-04 18:55:52 +0900

    [21.12.04] header 파일을 통한 클래스 구현부 선언부 main()부 나누기

---
+++ 02_C++_Class and Object_11/Circle.cpp
@@ -0,0 +1,23 @@
+/*
+클래스의 구현부 선언 
+*/
+#include <iostream>
+using namespace std;
+
+#include "Circle.h"
+
+Circle::Circle() {
+	radius = 1;
+	cout << "반지름 " << radius;
+	cout << " 원 생성" << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "반지름 " << radius;
+	cout << " 원 생성" << endl;
+}
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
