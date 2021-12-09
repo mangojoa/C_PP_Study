769883236fc5d17f85363c7d6b62086a4a8993ed
mangojoa <sxfc675@gmail.com>
2021-12-10 00:34:54 +0900

    [21.12.10] string 문자열 키 입력 응용

--- 3장_C++_Pointer_Array_Memory_10/21.12.10_01.cpp
+++ 3장_C++_Pointer_Array_Memory_10/21.12.10_01.cpp
@@ -38,6 +38,7 @@ delete p; 스트링 객체 반환
 
 using namespace std;
 
+/*
 int main() {
 	string str;
 	string address("광주 산수동 516-5");
@@ -50,4 +51,24 @@ int main() {
 	cout << address << endl;
 	cout << copyAddress << endl;
 	cout << title << endl;
+}
+*/
+
+// 응용
+int main() {
+	string name[5];
+
+	for (int i = 0; i < 5; i++) {
+		cout << "name >> ";
+		getline(cin, name[i], '\n');
+	}
+
+	string latter = name[0];
+	for (int i = 0; i < 5; i++) {
+		if (latter < name[i]) {
+			latter = name[i];
+		}
+	}
+
+	cout << "사전에서 가장 뒤에 나오는 문자열은 " << latter << endl;
 }
