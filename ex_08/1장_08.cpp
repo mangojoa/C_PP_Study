9c21145045a99220030ac2289f4aa9fcf0fde0b3
mangojoa <sxfc675@gmail.com>
2022-04-14 22:42:05 +0900

    [22.04.14] 문제풀이

--- ex_08/1장_08.cpp
+++ ex_08/1장_08.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
+	string test;
 	char name[100];
 	int cnt = 0, max = 0;
 
 	cout << "5 명의 이름을 ';'으로 구분하여 입력하세요!" << endl;
 
-	for (int a = 0; a < 5; a++) {
+	for (int a = 1; a < 6; a++) {
 		cin.getline(name, 100, ';');
-		cout << a + 1 << ' : ' << name << endl;
-		
-	
-	
+		cout << a << ' : ' << name << endl;
+
+		if (max < strlen(name)) {
+			max = strlen(name);
+			cnt = a;
+			test = name;
+		}
 	}
 
+	cout << "가장 긴 이름은 : " << test << '\n';
 
 }
