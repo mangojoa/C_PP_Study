9005ddbe240de3cb08e67f3db0007082ae27823a
mangojoa <sxfc675@gmail.com>
2022-05-10 23:45:35 +0900

    [22.05.10] vector 에 활용되는 함수 사용해보기

--- 9장_Template_STL_12/22.05.10_01.cpp
+++ 9장_Template_STL_12/22.05.10_01.cpp
@@ -67,3 +67,28 @@ operator[]()						지정된 원소에 대한 참조 리턴
 operator=()							이 백터를 다른 백터에 치환(복사)
 */
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> v;
+
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << '\t';
+	}
+	cout << endl;
+
+	v[0] = 10;
+	int m = v[2];
+	v.at(2) = 5;
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << '\t';
+	}
+	cout << endl;
+}
