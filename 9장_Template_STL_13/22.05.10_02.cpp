0986f8355b1ff364a5e86f629525f953abd3db5b
mangojoa <sxfc675@gmail.com>
2022-05-11 23:51:46 +0900

    [22.05.11] 문자열을 저장하는 백터구현 코드 수정

--- 9장_Template_STL_13/22.05.10_02.cpp
+++ 9장_Template_STL_13/22.05.10_02.cpp
@@ -6,20 +6,20 @@
 using namespace std;
 
 int main() {
-	vector<string> sv;
-	string name;
+	vector<string> sv; 
+	string name; 
 
-	cout << "이름을 5개 입력해봐 >> " << endl;
-	for (int i = 0; i < 5; i++) {
-		cout << i + 1 << ">>";
+	cout << "이름을 5개 입력해봐 >> " << endl; 
+	for (int i = 0; i < 5; i++) { 
+		cout << i + 1 << ">>"; 
 		getline(cin, name);
-		sv.push_back(name);
+		sv.push_back(name); 
 	}
 
-	name = sv.at(0); 
-	for (int i = 1; i < sv.size(); i++) {
-		if (name < sv[i]) name = sv[i];
+	name = sv.at(0);  
+	for (int i = 1; i < sv.size(); i++) { 
+		if (name < sv[i]) name = sv[i];  
 	}
 
-	cout << "사전에서 가장 뒤에 나오는 이름은 [" << name << "] 입니다" << endl;
+	cout << "사전에서 가장 뒤에 나오는 이름은 [" << name << "] 입니다" << endl; 
 }
