62cd4b9ec3e23e74b163867f5bd7946b2bbe25e6
mangojoa <sxfc675@gmail.com>
2022-05-21 23:47:57 +0900

    [22.05.21] iterator 예시 코드 구현

--- 9장_Template_STL_16/22.05.20_01.cpp
+++ 9장_Template_STL_16/22.05.20_01.cpp
@@ -28,3 +28,27 @@ for (it =v.begin(); it != v.end(); it++) {
 }
 
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> v; // create vector int type
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
+	vector<int>::iterator it; // 백터 v의 원소에 대한 포인터 it 선언
+
+	for (it = v.begin(); it != v.end(); it++) {
+		int n = *it;
+		n = n * 2;
+		*it = n;
+	}
+
+	for (it = v.begin(); it != v.end(); it++) {
+		cout << "it" << ' ';
+	}
+	cout << endl;
+}
