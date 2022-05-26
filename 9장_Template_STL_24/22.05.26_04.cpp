6a7c4655a277fb7f6533d6aef5d35e55ba4d69f0
mangojoa <sxfc675@gmail.com>
2022-05-26 20:21:22 +0900

    [22.05.26] 람다식을 이용한 for_each() 함수 간편화

--- 9장_Template_STL_24/22.05.26_04.cpp
+++ 9장_Template_STL_24/22.05.26_04.cpp
@@ -5,12 +5,14 @@
 #include <algorithm>
 using namespace std;
 
-void print(int n) {
-	cout << n << " ";
-}
+//void print(int n) {
+// 	cout << n << " ";
+//}
+// 람다식 사용으로 인해 이제 void print(int n)은 사용하지 않아도 된다.
 
 int main() {
 	vector <int> v = { 1,2,3,4,5,6,7,8,9 };
 
-	for_each(v.begin(), v.end(), print);
+	// 여기에 람다식을 사용한다면 ?
+	for_each(v.begin(), v.end(), [](int n) {cout << n << " ";});
 }
