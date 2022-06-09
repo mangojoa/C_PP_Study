33d35041471a5d4cfac1544a9c95bee903ebeb31
mangojoa <sxfc675@gmail.com>
2022-06-09 23:48:08 +0900

    [22.06.09] 포멧 플래그의 개념

---
+++ 10장_String_input_output_System_05/22.06.09_01.cpp
@@ -0,0 +1,37 @@
+/* [22.06.09] 포멧 입출력
+## 포멧플래그?
+C++ 입출력 스트림은 32개의 포멧 플래스를 저장하는 맴버 변수를 두고, 이를 참조하여 포멧 입출력한다.
+포멧 플래그는 모든 입출력 스트림에서 공통으로 사용되므로, ios클래스에 정수형 상수로 정의되어 있다. 
+
+하나의 플래스는 한 비트로 표현되며 한 가지 포멧 정보를 표현한다.
+cin 이나 cout은 입출력 시 이 포멧 변수에 세팅된 플래그 값을 방여하여 포멧 입출력을 수행한다. 
+
+그러므로 포멧 변수를 적절히 설정하면 입출력 포멧을 제어할 수 있다. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << 30 << endl;
+
+	cout.unsetf(ios::dec);
+	cout.setf(ios::hex);
+	cout << 30 << endl;
+
+	cout.setf(ios::showbase);
+	cout << 30 << endl;
+
+	cout.setf(ios::uppercase);
+	cout << 30 << endl;
+
+	cout.setf(ios::dec | ios::showpoint);
+	cout << 23.5 << endl;
+
+	cout.setf(ios::scientific);
+	cout << 23.5 << endl;
+
+	cout.setf(ios::showpos);
+	cout << 23.5 << endl;
+
+}
